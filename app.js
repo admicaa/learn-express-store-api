cabc1925f4404fed3a4fe8ecdd5a6cf907b9b46c
admica <almestaadmica@gmail.com>
2023-05-02 01:30:52 +0300

    MY CUSTOM INIT

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log('04 Store API')
+import express from "express";
+import connector from "./database/connector.js";
+import RouteServiceProvider from "./providers/RouteServiceProvider.js";
+import notFound from "./app/middleware/not-found.js";
+import errorHandlerMiddleware from "./app/middleware/error-handler.js";
+
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+    this.server.use(notFound);
+    this.server.use(errorHandlerMiddleware);
+  }
+
+  middlewares() {
+    this.server.use(express.static("public"));
+    this.server.use(express.json());
+  }
+
+  routes() {
+    new RouteServiceProvider(this.server);
+  }
+}
+
+var server = new App().server;
+
+var port = process.env.PORT || 3000;
+
+connector
+  .mongo()
+  .then((result) => {
+    server.listen(port, () => {
+      console.log(
+        `Started Express server at port ${port} \n you can run it clicking this link http://localhost:${port}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.error(`Failed to connect to DB ${err}`);
+  });
